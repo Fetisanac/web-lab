bd7a5cedfebaa9ca775eb88db4fb31255c8ce420
Fetisanac <147306237+Fetisanac@users.noreply.github.com>
2026-03-03 09:28:05 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App() {
       <h2>LAB-1</h2>
       <p>Ad Soyad: Feti Sanac</p>
       <p>Ogrenci No: 230541105</p>
+      <hr />
+      <h3>Hakkımda</h3>
+      <p>Bölüm:Yazılım Mühendisliği</p>
+      <p>Hobiler ve İlgi Alanları: Web geliştirme ve yazılım mimarisi üzerine çalışmak. Arta kalan zamanlarda One Piece ve Demon Slayer gibi animeleri izlemek.</p>
     </div>
   )
 }
